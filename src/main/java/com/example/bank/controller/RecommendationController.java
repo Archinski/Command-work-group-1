79a9286b12ddc327dc8bd2fb5c2c793ac29f455f
asdtt <asdttooppp@gmail.com>
2024-12-01 22:55:12 +0500

    Correct project

--- src/main/java/com/example/bank/controller/RecommendationController.java
+++ src/main/java/com/example/bank/controller/RecommendationController.java
@@ -2,9 +2,13 @@ package com.example.bank.controller;
 
 import com.example.bank.dto.RecommendationResponse;
 import com.example.bank.service.RecommendationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 public class RecommendationController {
@@ -17,6 +21,12 @@ public class RecommendationController {
 
     @GetMapping("/recommendation/{user_id}")
     public RecommendationResponse getRecommendations(@PathVariable("user_id") String userId) {
+        try {
+            UUID.fromString(userId); // Проверка на корректность UUID
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user ID format");
+        }
+
         return recommendationService.getRecommendations(userId);
     }
 }
