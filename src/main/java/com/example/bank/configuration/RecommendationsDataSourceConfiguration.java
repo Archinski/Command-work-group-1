731b5df53dd04a079b56818bda182cc929c08f9b
asdtt <asdttooppp@gmail.com>
2024-12-16 21:02:52 +0500

    3rd commit

--- src/main/java/com/example/bank/configuration/RecommendationsDataSourceConfiguration.java
+++ src/main/java/com/example/bank/configuration/RecommendationsDataSourceConfiguration.java
@@ -3,8 +3,10 @@ package com.example.bank.configuration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -14,14 +16,11 @@ public class RecommendationsDataSourceConfiguration {
 
     @Bean(name = "recommendationsDataSource")
     public DataSource recommendationsDataSource(
-            @Value("${spring.datasource.url}") String url,
-            @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password
-    ) {
+            @Value("${spring.rules.url}") String url
+
+            ) {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setReadOnly(true);
         return dataSource;
@@ -30,5 +29,17 @@ public class RecommendationsDataSourceConfiguration {
     @Bean(name = "recommendationsJdbcTemplate")
     public JdbcTemplate recommendationsJdbcTemplate(@Qualifier("recommendationsDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    @Qualifier("mainDataSourceProperties")
+    public DataSourceProperties mainDataSrcProp(){
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean
+    public DataSource mainDataSrc(@Qualifier("mainDataSourceProperties") DataSourceProperties dataSourceProperties){
+        DataSource src = dataSourceProperties.initializeDataSourceBuilder().build();
+        return src;
     }
 }
